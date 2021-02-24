dba201e233c561d01788e840b6c0662e4a89ba43
Zuro <79481914+Zurocoin@users.noreply.github.com>
2021-02-24 15:43:06 +0000

    Update CryptoNoteConfig.hpp

--- src/CryptoNoteConfig.hpp
+++ src/CryptoNoteConfig.hpp
@@ -106,7 +106,7 @@ const uint32_t P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT      = 5000;           // 5 
 const char P2P_STAT_TRUSTED_PUB_KEY[] = "E29507CA55455F37A3B783EE2C5123B8B6A34A0C5CAAE050922C6254161480C1";
 
 const char *const SEED_NODES[] = {
-    "127.0.0.1:7070"};
+    };
 
 struct CheckpointData {
 	uint32_t index;
